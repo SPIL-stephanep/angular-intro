dfdb32b109012ffab4a1e905db46fc5ee30f9215
Stephane P. Pericat <stephane.pericat@spilgames.com>
2014-02-25 12:54:03 +0100

    NEW factories

--- public/js/demo.js
+++ public/js/demo.js
@@ -5,11 +5,27 @@ var app = angular.module('intro', ['ui.router']);
 app.controller('HomeCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
     $scope.pageTitle = 'This is the home page';
     $scope.counter = 0;
+    $scope.myInput = 'Two way binding';
 
     $scope.multiply = function(a, b) {
         console.log(a * b);
     }
 }]);
+
+app.controller('FactoryCtrl', ['$scope', 'Portals', function($scope, Portals) {
+    $scope.portals = Portals;
+}]);
+
+// Factories
+app.factory('Portals', function() {
+    return [
+        {name: 'gamesgames.com', country: 'USA'},
+        {name: 'jetztspielen.de', country: 'Germany'},
+        {name: 'spelletjes.nl', country: 'NL'},
+        {name: 'zapjuegos.com', country: 'Mexico'},
+        {name: 'jeux.fr', country: 'France'}
+    ];
+});
 
 // Setup the view routes
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -22,7 +38,23 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
             url: "/",
             templateUrl: "/templates/home.html",
             controller: 'HomeCtrl'
-        });
+        })
+        .state('Factories', {
+            url: "/factories",
+            templateUrl: "/templates/factories.html",
+            controller: 'FactoryCtrl'
+        })
+        // .state('Home', {
+        //     url: "/",
+        //     templateUrl: "/templates/home.html",
+        //     controller: 'HomeCtrl'
+        // })
+        // .state('Home', {
+        //     url: "/",
+        //     templateUrl: "/templates/home.html",
+        //     controller: 'HomeCtrl'
+        // })
+        ;
 
     $locationProvider.hashPrefix('!');
 });
